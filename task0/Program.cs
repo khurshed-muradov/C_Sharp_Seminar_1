dc07563e9c04b1afcea754a9aa5bd862c3fa5a00
khurshed <hurshed@gmail.com>
2023-07-07 00:12:01 +0500

    Добавил задачу вычисления квадрата числа

--- task0/Program.cs
+++ task0/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int result = number * number;
+Console.Write("Квадрат числа " + number + " = " + result);
+
+
