db40c29090140cd933ad90ccb32707f97f78f868
khurshed <hurshed@gmail.com>
2023-07-08 23:34:11 +0500

    Добавил опасание второго задания

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Введите числа: ");
+﻿// Напишите программу. которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+Console.WriteLine("Введите числа: ");
 int number1 = int.Parse(Console.ReadLine());
 int number2 = int.Parse(Console.ReadLine());
 if (number1 == number2 * number2)
