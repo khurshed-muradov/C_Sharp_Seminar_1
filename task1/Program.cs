ae26facd558e979fb4154f95b3cd2fff3e054f5a
khurshed <hurshed@gmail.com>
2023-07-08 17:26:08 +0500

    Задача - Первое число квадрат второго или нет

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите числа: ");
+int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+if (number1 == number2 * number2)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
